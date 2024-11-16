b1e49bee73af708eeb2902aafb2a347e7be8b3ae
Ian Hill <129342357+ianhill4@users.noreply.github.com>
2024-11-16 15:23:11 -0500

    Update background.js

--- src/background/background.js
+++ src/background/background.js
@@ -1,1 +1,15 @@
 const LEETCODE_BASE_URL = "https://leetcode.com"
+
+//Check if URL contains "/submissions/detail/" & "/check/"
+
+//Check if response has "state" == "SUCCESS" 
+
+//increase total number of attempts
+
+// and if "status_msg" == "Accepted"
+
+//increaese successes
+
+//make background green
+
+//record "lang", "status_runtime", "status_memory"
