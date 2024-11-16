25ca713c094553c887feab46735be4d95c907fad
Ian Hill <129342357+ianhill4@users.noreply.github.com>
2024-11-16 12:49:16 -0500

    Update popup.js

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,9 +2,9 @@ const leaderboardButton = document.getElementById("leaderboardButton");
 const friendsButton = document.getElementById("friendsButton");
 const challengesButton = document.getElementById("challengesButton");
 
-leaderboardButton = addEventListener("click", switchToLeaderboard);
-friendsButton = addEventListener("click", switchToFriends);
-challengesButton = addEventListener("click", switchToChallenges);
+leaderboardButton.addEventListener("click", switchToLeaderboard);
+friendsButton.addEventListener("click", switchToFriends);
+challengesButton.addEventListener("click", switchToChallenges);
 
 function switchToLeaderboard(){
     switchTab("leaderboard");
@@ -18,9 +18,11 @@ function switchToChallenges(){
     switchTab("challenges");
 }
 
-//const leaderboardDiv = document.getElementsById("leaderboard");
-//const friendsDiv = document.getElementsById("friends");
-//const challengesDiv = document.getElementsById("challenges");
+//#const leaderboardDiv = document.getElementsById("leaderboard");
+//#const friendsDiv = document.getElementsById("friends");
+//#const challengesDiv = document.getElementsById("challenges");
+
+const tabDivs = []
 
 function switchTab(tab){
     switch(tab){
