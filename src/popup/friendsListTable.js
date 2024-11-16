b8d7561e4dfb2cd9f12d11c877c78811e32243df
Ian Hill <129342357+ianhill4@users.noreply.github.com>
2024-11-16 13:53:35 -0500

    Update friendsListTable.js

--- src/popup/friendsListTable.js
+++ src/popup/friendsListTable.js
@@ -1,5 +1,5 @@
-const friendsListTable = document.getElementById("friendsListTableDiv");
-
+const friendsListTableDiv = document.getElementById("friendsListTableDiv");
+/*
 const friends = ["Bob","Alice","Henry Cramer","etock"];
 const streaks = ["0","2","-5","3"];
 
@@ -28,3 +28,28 @@ function generateTable(){
 }
 
 generateTable();
+*/
+
+//setup our table array
+var tableArr = [
+  ["row 1, cell 1", "row 1, cell 2"],
+  ["row 2, cell 1", "row 2, cell 2"]
+]
+
+//create a Table Object
+let table = document.createElement('table');
+//iterate over every array(row) within tableArr
+for (let row of tableArr) {
+//Insert a new row element into the table element
+  table.insertRow();
+//Iterate over every index(cell) in each array(row)
+  for (let cell of row) {
+//While iterating over the index(cell)
+//insert a cell into the table element
+    let newCell = table.rows[table.rows.length - 1].insertCell();
+//add text to the created cell element
+    newCell.textContent = cell;
+  }
+}
+//append the compiled table to the DOM
+friendsListTableDiv.appendChild(table);
