7ab8e21637124a7cc7c3042b3c497b9ecf612c3f
Ian Hill <129342357+ianhill4@users.noreply.github.com>
2024-11-16 13:33:06 -0500

    Update leaderboardTables.js

--- src/popup/leaderboardTables.js
+++ src/popup/leaderboardTables.js
@@ -6,7 +6,7 @@ const names = ["ihill", "jtrokel", "lsciortino", "etock"];
 const times = [40, 50, 20, 25];
 const spaces = [4, 2, 10, 5];
 const problems = [1, 2, 3, 4];
-const columns = [names, times, spaces, problems]
+const columns = [names, times, spaces, problems];
 
 function generateTables() {
     friendsTable.innerHTML += "<tr>";
@@ -19,7 +19,7 @@ function generateTables() {
         friendsTable.innerHTML += "<tr>";
 
         for(let j = 0; j < columns.length; j++) {
-            friendsTable.innerHTML += `<td>${columns[j][i]}</td>`
+            friendsTable.innerHTML += `<td>${columns[j][i]}</td>`;
         }
 
         friendsTable.innerHTML += "</tr>";
