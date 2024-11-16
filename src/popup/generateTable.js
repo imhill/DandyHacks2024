061fe5cb6111e42a8d27b4b81e73ef0579104807
Ian Hill <129342357+ianhill4@users.noreply.github.com>
2024-11-16 16:26:40 -0500

    Create generateTable.js

---
+++ src/popup/generateTable.js
@@ -0,0 +1,21 @@
+export function generateTable(){
+    const table = document.createElement('table');
+    
+    const headerRow = table.insertRow();
+    
+    for (const key in data[0]) {
+        const headerCell = headerRow.insertCell();
+        headerCell.textContent = key;
+    }
+    
+    for (const rowData of data) {
+        const row = table.insertRow();
+        
+        for (const key in rowData) {
+            const cell = row.insertCell();
+            cell.textContent = rowData[key];
+        }
+    }
+    
+    return table;
+}
