f18bc40d0925d5e798b9111a84619b383060dacc
Ian Hill <129342357+ianhill4@users.noreply.github.com>
2024-11-16 16:22:51 -0500

    Update friendsListTable.js

--- src/popup/friendsListTable.js
+++ src/popup/friendsListTable.js
@@ -1,14 +1,12 @@
 export function createFriendsListTable(data) {
   const table = document.createElement('table');
 
-  // Create table header
   const headerRow = table.insertRow();
   for (const key in data[0]) {
     const headerCell = headerRow.insertCell();
     headerCell.textContent = key;
   }
 
-  // Create table rows
   for (const rowData of data) {
     const row = table.insertRow();
     for (const key in rowData) {
