bc000682faf1003a347b1fafc83eb9ea4ff07da9
Ezra Tock <etock@u.rochester.edu>
2024-11-16 15:07:38 -0500

    update database to users

--- src/hello.js
+++ src/hello.js
@@ -30,7 +30,7 @@ const server = http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.end("PONG\n");
   } else if (req.url === '/postgres-test') {
-    client.query('SELECT * FROM test', (err, result) => {
+    client.query('SELECT * FROM users', (err, result) => {
       if (err) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Error querying the database\n');
