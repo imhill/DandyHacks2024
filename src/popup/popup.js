76d2dcb9a83734cf9c8c75505a319178e35f6bf4
Ian Hill <129342357+ianhill4@users.noreply.github.com>
2024-11-16 12:31:00 -0500

    Update popup.js

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,4 +1,26 @@
 function switchTo(evt, tabName) {
+    const leaderboardButton = document.getElementsById("leaderboardButton");
+    const friendsButton = document.getElementsById("friendsButton");
+    const challengesButton = document.getElementsById("challengesButton");
+
+    leaderboardButton = addEventListener("click", function() { switchTab("leaderboard"); });
+    friendsButton = addEventListener("click", function() { switchTab("friends"); });
+    challengesButton = addEventListener("click", function() { switchTab("challenges"); });
+
+    function switchTab(tab){
+        switch tab{
+            case "leaderboard":
+                console.log("L");
+                break
+            case "freinds":
+                console.log("F");
+                break
+            case "challenges":
+                console.log("C");
+                break
+        }
+    }
+    
     var tabcontent = document.getElementsByClassName("tabcontent");
 
     for (tab of tabcontent) { 
