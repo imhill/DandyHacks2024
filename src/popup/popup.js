f07c3c9ca33f5946b09a282fbbc6da4212bb0caa
Ian Hill <129342357+ianhill4@users.noreply.github.com>
2024-11-16 12:47:42 -0500

    Update popup.js

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,48 +1,37 @@
-function switchTo(evt, tabName) {
-    const leaderboardButton = document.getElementsById("leaderboardButton");
-    const friendsButton = document.getElementsById("friendsButton");
-    const challengesButton = document.getElementsById("challengesButton");
+const leaderboardButton = document.getElementsById("leaderboardButton");
+const friendsButton = document.getElementsById("friendsButton");
+const challengesButton = document.getElementsById("challengesButton");
 
-    leaderboardButton = addEventListener("click", switchToLeaderboard);
-    friendsButton = addEventListener("click", switchToFriends);
-    challengesButton = addEventListener("click", switchToChallenges);
+leaderboardButton = addEventListener("click", switchToLeaderboard);
+friendsButton = addEventListener("click", switchToFriends);
+challengesButton = addEventListener("click", switchToChallenges);
 
-    function switchToLeaderboard(){
-        switchTab("leaderboard");
+function switchToLeaderboard(){
+    switchTab("leaderboard");
+}
+
+function switchToFriends(){
+    switchTab("friends");
+}
+
+function switchToChallenges(){
+    switchTab("challenges");
+}
+
+//const leaderboardDiv = document.getElementsById("leaderboard");
+//const friendsDiv = document.getElementsById("friends");
+//const challengesDiv = document.getElementsById("challenges");
+
+function switchTab(tab){
+    switch(tab){
+        case "leaderboard":
+            console.log("L");
+            break
+        case "friends":
+            console.log("F");
+            break
+        case "challenges":
+            console.log("C");
+            break
     }
-
-    function switchToFriends(){
-        switchTab("friends");
-    }
-
-    function switchToChallenges(){
-        switchTab("challenges");
-    }
-
-    //const leaderboardDiv = document.getElementsById("leaderboard");
-    //const friendsDiv = document.getElementsById("friends");
-    //const challengesDiv = document.getElementsById("challenges");
-
-    function switchTab(tab){
-        switch(tab){
-            case "leaderboard":
-                console.log("L");
-                break
-            case "freinds":
-                console.log("F");
-                break
-            case "challenges":
-                console.log("C");
-                break
-        }
-    }
-    
-    var tabcontent = document.getElementsByClassName("tabcontent");
-
-    for (tab of tabcontent) { 
-        tab.style.display = "none";
-    }
-
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
 }
