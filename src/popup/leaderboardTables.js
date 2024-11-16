201466fdb3b7dfa2a1c91c51c0842ca4aa55eb56
Jacob Trokel <jtrokel@u.rochester.edu>
2024-11-16 13:24:08 -0500

    DYNAMIC TABLE GEN MARK 1 (WOWIEEEE)

---
+++ src/popup/leaderboardTables.js
@@ -0,0 +1,29 @@
+const friendsTable = getElementById("leaderboardTableFriends");
+const communityTable = getElementById("leaderboardTableCommunity");
+
+const headers = ["Name", "Runtime", "Space", "Problems"];
+const names = ["ihill", "jtrokel", "lsciortino", "etock"];
+const times = [40, 50, 20, 25];
+const spaces = [4, 2, 10, 5];
+const problems = [1, 2, 3, 4];
+const columns = [names, times, spaces, problems]
+
+function generateTables() {
+    friendsTable.innerHTML += "<tr>";
+    for(const h of headers) {
+        friendsTable.innerHTML += `<th>${h}</th>`;
+    }
+    friendsTable.innerHTML += "</tr>;
+
+    for(let i = 0; i < names.length; i++) {
+        friendsTable.innerHTML += "<tr>";
+
+        for(let j = 0; j < columns.length; j++) {
+            friendsTable.innerHTML += `<td>${columns[j][i]}</td>`
+        }
+
+        friendsTable.innerHTML += "</tr>";
+    }
+}
+
+generateTables();
