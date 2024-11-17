05b6b7e8114f87015bcdcd379f2be74817bd75c8
Ezra Tock <etock@u.rochester.edu>
2024-11-17 04:18:16 -0500

    yertimus

--- src/hello.js
+++ src/hello.js
@@ -301,7 +301,7 @@ app.get('/get-problem-stats', async (req, res) => {
   if (!queryParm.titleSlug && queryParm.titleSlug) {
     return res.status(400).json({ error: 'Missing required query parameter: username' });
   }
-  const query = `SELECT p.*
+  const query = `SELECT (p.runtime, p.space, p.completed_at, u.username)
                   FROM problems p
                          JOIN friends f
                               ON p.usr_id = f.friend_id
@@ -326,6 +326,7 @@ app.get('/get-problem-stats', async (req, res) => {
                   FROM problems p
                          JOIN users u
                               ON u.usr_id = p.usr_id
+
                   WHERE p.usr_id = (
                     SELECT usr_id
                     FROM users
