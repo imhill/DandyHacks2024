eaacc3addc53ea4aa5dba786745948b60bc9b484
Ezra Tock <etock@u.rochester.edu>
2024-11-16 22:00:58 -0500

    add friend

--- src/hello.js
+++ src/hello.js
@@ -27,38 +27,9 @@ client.connect((err) => {
   }
 });
 
-// Example POST endpoint
-app.post('/post-problem', async (req, res) => {
-  // Access query parameters and JSON body
-  const queryParams = req.query;
-  const jsonBody = req.body;
 
-  // Validate query parameters and body
-  if (!queryParams.username) {
-    return res.status(400).json({ error: 'Missing required query parameter: username' });
-  }
-
-  if (!jsonBody || !jsonBody.problemNum || !jsonBody.runtime || !jsonBody.space ) {
-    return res.status(400).json({ error: 'Missing required JSON field' });
-  }
-  const query = `
-    INSERT INTO problems (usr_id, problem_number, runtime, space)
-    VALUES ((SELECT usr_id FROM users WHERE username = $1), $2, $3, $4)
-    ON CONFLICT (usr_id, problem_number)
-    DO UPDATE SET runtime = EXCLUDED.runtime, space = EXCLUDED.space;
-  `;
-  try {
-    // Execute the query with parameters
-    const result = await client.query(query, [queryParams.username, jsonBody.problemNum, jsonBody.runtime, jsonBody.space]);
-    // Send the response
-    res.sendStatus(201);
-  } catch (err) {
-    console.error('Error executing query:', err.stack);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
 //Create User Endpoint
-app.post('/add-friend', async (req, res) => {
+app.post('/create-user', async (req, res) => {
   // Access query parameters and JSON body
   const queryParams = req.query;
   const jsonBody = req.body;
@@ -71,12 +42,74 @@ app.post('/add-friend', async (req, res) => {
   const query = `
     INSERT INTO users (username)
     VALUES ($1) ON CONFLICT (username) DO NOTHING
-	RETURNING usr_id
   `;
   try {
     // Execute the query with parameters
     const result = await client.query(query, [queryParams.username]);
-    console.log(result);
+    res.sendStatus(201);
+  } catch (err) {
+    console.error('Error executing query:', err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/add-friend', async (req, res) => {
+  // Access query parameters and JSON body
+  const queryParams = req.query;
+  const jsonBody = req.body;
+  
+  // Validate query parameters and body
+  if (!queryParams.username) {
+    return res.status(400).json({ error: 'Missing required query parameter: username' });
+  }
+  
+  if (!jsonBody || !jsonBody.friend ) {
+    return res.status(400).json({ error: 'Missing JSON field friend' });
+  }
+  const query = `
+    INSERT INTO friends (usr_id, friend_id)
+    VALUES (
+               (SELECT usr_id FROM users WHERE username = $1),  -- Get the usr_id based on the username
+               (SELECT usr_id FROM users WHERE username = $2)   -- Get the friend_id based on the friend's username
+           )
+      ON CONFLICT (usr_id, friend_id)  -- Avoid conflicts if the same friendship already exists
+    DO NOTHING;
+  `;
+  try {
+    // Execute the query with parameters
+    const result = await client.query(query, [queryParams.username, jsonBody.friend]);
+    // Send the response
+    res.sendStatus(201);
+  } catch (err) {
+    console.error('Error executing query:', err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+
+app.post('/post-problem', async (req, res) => {
+  // Access query parameters and JSON body
+  const queryParams = req.query;
+  const jsonBody = req.body;
+  
+  // Validate query parameters and body
+  if (!queryParams.username) {
+    return res.status(400).json({ error: 'Missing required query parameter: username' });
+  }
+  
+  if (!jsonBody || !jsonBody.problemNum || !jsonBody.runtime || !jsonBody.space ) {
+    return res.status(400).json({ error: 'Missing required JSON field' });
+  }
+  const query = `
+    INSERT INTO problems (usr_id, problem_number, runtime, space)
+    VALUES ((SELECT usr_id FROM users WHERE username = $1), $2, $3, $4)
+    ON CONFLICT (usr_id, problem_number)
+    DO UPDATE SET runtime = EXCLUDED.runtime, space = EXCLUDED.space;
+  `;
+  try {
+    // Execute the query with parameters
+    const result = await client.query(query, [queryParams.username, jsonBody.problemNum, jsonBody.runtime, jsonBody.space]);
     // Send the response
     res.sendStatus(201);
   } catch (err) {
