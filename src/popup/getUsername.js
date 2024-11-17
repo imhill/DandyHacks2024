bd83f17742e51b7483f0001ea9f895474a30262f
Ian Hill <129342357+ianhill4@users.noreply.github.com>
2024-11-17 02:55:36 -0500

    Create getUsername.js

---
+++ src/popup/getUsername.js
@@ -0,0 +1,22 @@
+export async function getUsername() {
+    try {
+        const query = `
+            query globalData {
+              userStatus {
+                username
+              }
+             }`;
+        const response = await fetch("https://leetcode.com/graphql/", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ query: query })
+        });
+        const data = await response.json();
+        const username = data.data.userStatus.username;
+        return username;
+    } catch (err) {
+        console.error(err);
+    }
+}
