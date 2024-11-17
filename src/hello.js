57e81f7cdc853b3dbe46cb20e30045d6a501c143
jtrokel <118480689+jtrokel@users.noreply.github.com>
2024-11-17 00:01:26 -0500

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -146,7 +146,11 @@ app.post('/post-problem', async (req, res) => {
     // Execute the query with parameters
     const result = await client.query(query, [queryParams.username, jsonBody.problemNum, jsonBody.runtime, jsonBody.space]);
     // Send the response
-    res.set('Access-Control-Allow-Origin', '*');
+    res.set({
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+    });
     res.sendStatus(201);
   } catch (err) {
     console.error('Error executing query:', err.stack);
