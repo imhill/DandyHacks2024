4d0d515e27c0bb497ee21adad389940d1082d345
Ezra Tock <etock@u.rochester.edu>
2024-11-16 21:21:10 -0500

    add_friend attempt 1

--- src/hello.js
+++ src/hello.js
@@ -49,18 +49,72 @@ app.post('/post-problem', async (req, res) => {
   `;
   try {
     // Execute the query with parameters
-    console.log("befoer query");
     const result = await client.query(query, [queryParams.username, jsonBody.problemNum, jsonBody.runtime, jsonBody.space]);
-    console.log("after query");
     // Send the response
     res.sendStatus(201);
-    console.log("after response");
   } catch (err) {
     console.error('Error executing query:', err.stack);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
+app.post('/post-problem', async (req, res) => {
+  // Access query parameters and JSON body
+  const queryParams = req.query;
+  const jsonBody = req.body;
+  
+  // Validate query parameters and body
+  if (!queryParams.username) {
+    return res.status(400).json({ error: 'Missing required query parameter: username' });
+  }
+  
+  if (!jsonBody || !jsonBody.problemNum || !jsonBody.runtime || !jsonBody.space ) {
+    return res.status(400).json({ error: 'Missing required JSON field' });
+  }
+  const query = `
+    INSERT INTO problems (usr_id, problem_number, runtime, space)
+    VALUES ((SELECT usr_id FROM users WHERE username = $1), $2, $3, $4)
+    ON CONFLICT (usr_id, problem_number)
+    DO UPDATE SET runtime = EXCLUDED.runtime, space = EXCLUDED.space;
+  `;
+  try {
+    // Execute the query with parameters
+    const result = await client.query(query, [queryParams.username, jsonBody.problemNum, jsonBody.runtime, jsonBody.space]);
+    // Send the response
+    res.sendStatus(201);
+  } catch (err) {
+    console.error('Error executing query:', err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/add_friend', async (req, res) => {
+  // Access query parameters and JSON body
+  const queryParams = req.query;
+  const jsonBody = req.body;
+  
+  // Validate query parameters and body
+  if (!queryParams.username) {
+    return res.status(400).json({ error: 'Missing required query parameter: username' });
+  }
+  
+  if (!jsonBody || !jsonBody.friend ) {
+    return res.status(400).json({ error: 'Missing JSON field friend' });
+  }
+  const query = `
+    INSERT INTO friends (usr_id, friend_id)
+    VALUES ($1, $2)
+  `;
+  try {
+    // Execute the query with parameters
+    const result = await client.query(query, [queryParams.username, jsonBody.friend]);
+    // Send the response
+    res.sendStatus(201);
+  } catch (err) {
+    console.error('Error executing query:', err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 // Prints a log once the server starts listening
 app.listen(port, hostname, function () {
