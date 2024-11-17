80faf90fe2c7cb7a9d081f557f6727a899e02fbc
Ezra Tock <etock@u.rochester.edu>
2024-11-16 23:34:47 -0500

    hardcode

--- src/hello.js
+++ src/hello.js
@@ -77,7 +77,7 @@ app.post('/add-friend', async (req, res) => {
   try {
     console.log(queryParams.username);
     // Execute the query with parameters
-    const userRes = await client.query(`SELECT usr_id FROM users WHERE username = $1::VARCHAR(50);`, [queryParams.username]);
+    const userRes = await client.query(`SELECT usr_id FROM users WHERE username = $1::VARCHAR;`, [queryParams.username]);
     const friendRes = await client.query(`SELECT usr_id FROM users WHERE username = ($2);`, [jsonBody.friend]);
   
     console.log(userRes.rows);
