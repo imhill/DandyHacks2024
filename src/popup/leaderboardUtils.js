0bd3856f5221522be794bb83a12cbc51bdb34085
Jacob Trokel <jtrokel@u.rochester.edu>
2024-11-17 03:13:52 -0500

    first test leaderboard

--- src/popup/leaderboardUtils.js
+++ src/popup/leaderboardUtils.js
@@ -1,5 +1,21 @@
 import {GetUsername} from "./getUsername.js";
 
 export async function GetLeaderboard() {
+    // Get title slug
+    let [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    });
+    const tabUrl = tab.url;
+    const slugRegex = /.*?leetcode.com\/problems\/(.*?)\/sub.*/;
+    const result = slugRegex.exec(tabUrl);
+
+    const titleSlug = result[1];
     
+    // Get username
+    const username = await GetUsername();
+    
+    // Send leaderboard request
+    console.log("slug, name, ", titleSlug, username);
+
 }
