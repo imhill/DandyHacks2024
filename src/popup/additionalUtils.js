b3cb5735383284f012401197cee93171a11f2aed
Ian <ianmhill4@gmail.com>
2024-12-22 23:13:10 -0700

    added popup for user deletion confirmation

---
+++ src/popup/additionalUtils.js
@@ -0,0 +1,3 @@
+export function ShowDeleteConfirmationModal(username){
+    alert(`Are you sure you want to remove ${username} as a friend?`);
+}
--- src/popup/generateTable.js
+++ src/popup/generateTable.js
@@ -1,3 +1,5 @@
+import {ShowDeleteConfirmationModal} from "./additionalUtils.js";
+
 export function GenerateTable({data, remove=false}){
     //create the table object
     const table = document.createElement('table');
@@ -16,23 +18,42 @@ export function GenerateTable({data, remove=false}){
     //create the body
     const tableBody = table.createTBody();
 
+    let rowNumber = 0;
+
     //iterate through the rows
     for (const rowData of data) {
         const row = tableBody.insertRow();
+
+        let cellNumber = 0;
 
         //within each row, add the data
         for (const key in rowData) {
             const cell = row.insertCell();
             cell.textContent = rowData[key];
+            cell.id = `row${rowNumber}.cell${cellNumber}`;
             if(remove){
                 const removeFriendButton = document.createElement("span");
                 removeFriendButton.classList = "removeFriendButton";
+                removeFriendButton.id = `row${rowNumber}`;
                 removeFriendButton.innerHTML = "&times;";
+                removeFriendButton.rowNumber = rowNumber;
+                removeFriendButton.username = rowData[key];
+                removeFriendButton.addEventListener("click",deleteConfirmation);
                 cell.appendChild(removeFriendButton);
             }
+
+            cellNumber++;
         }
+
+        rowNumber++;
     }
 
     //come on now you know what this is
     return table;
+}
+
+function deleteConfirmation(event){
+    const username = event.currentTarget.username;
+
+    ShowDeleteConfirmationModal(username);
 }
