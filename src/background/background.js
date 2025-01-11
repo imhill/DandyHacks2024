7026a33ef2d7b7913f05c6a37e172aab749b0ea3
Ian <ianmhill4@gmail.com>
2025-01-10 19:11:34 -0500

    fixing incorrect JSON error when runtime/memory equals 0 on problem submission

--- src/background/background.js
+++ src/background/background.js
@@ -7,13 +7,18 @@ chrome.webRequest.onCompleted.addListener(
     const url = details.url;
     const initiator = details.initiator;
     if (url.includes("/check/") && initiator == "https://leetcode.com") {
-      console.log("HOORAY!!!!");
+      //console.log("HOORAY!!!!!");
+      //console.log("Found check request from LeetCode");
 
       try {
         const response = await fetch(url);
         const data = await response.json();
 
+        //console.log("Data found");
+
         if (data.state == "SUCCESS" && data.status_msg == "Accepted") {
+          //console.log("Accepted submission found");
+
           id = data.submission_id;
           const query = `
             query {
@@ -46,10 +51,18 @@ chrome.webRequest.onCompleted.addListener(
 
           const gqlData = await gqlResponse.json();
 
+          //console.log("GQL response recieved");
+
           const gqlSubData = gqlData.data.submissionDetails;
 
           //console.log("user: ", gqlSubData.user.username, "lang: ", gqlSubData.lang.name, "runtime: ", gqlSubData.runtimeDisplay, "memory: ", gqlSubData.memoryDisplay);
           //console.log("slug: ", gqlSubData.question.titleSlug);
+
+          const createResp = await fetch(`http://3.143.223.90:8000/create-user?username=${gqlSubData.user.username}`, {
+            method: "POST"
+          });
+
+          //console.log("User creation query executed",createResp);
 
           const dbBody = JSON.stringify({
             titleSlug: gqlSubData.question.titleSlug,
@@ -57,10 +70,7 @@ chrome.webRequest.onCompleted.addListener(
             space: Number(gqlSubData.memory)
           });
 
-          const createResp = await fetch(`http://3.143.223.90:8000/create-user?username=${gqlSubData.user.username}`, {
-            method: "POST"
-          });
-          //console.log(createResp);
+          //console.log(dbBody);
 
           const dbResponse = await fetch(`http://3.143.223.90:8000/post-problem?username=${gqlSubData.user.username}`, {
             method: "POST",
@@ -69,7 +79,11 @@ chrome.webRequest.onCompleted.addListener(
             },
             body: dbBody
           });
-          //console.log(dbResponse);
+          
+          //console.log("Problem statistic query executed",dbResponse);
+
+        } else {
+          //console.log("Not an accepted submission");
         }
       } catch (err) {
         console.error(err);
